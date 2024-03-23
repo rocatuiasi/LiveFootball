4abd276a95aea566420659a74488fdd0171d8b17
Dragos Gherasim <dragos-george.gherasim@student.tuiasi.ro>
2024-03-23 13:25:58 +0200

    Created Main Window template for project initialization

---
+++ LiveFootball/LiveFootball/Views/LeaguesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace LiveFootball.Views;
+
+/// <summary>
+///     Interaction logic for Leagues.xaml
+/// </summary>
+public partial class LeaguesView : UserControl
+{
+    public LeaguesView()
+    {
+        InitializeComponent();
+    }
+}
