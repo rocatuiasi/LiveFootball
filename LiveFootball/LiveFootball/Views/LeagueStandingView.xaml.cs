806ff7d95fa49fe4ced9402a3defab6892c27233
Dragos Gherasim <dragos-george.gherasim@student.tuiasi.ro>
2024-03-27 20:37:53 +0200

    Created LeagueStandingView

---
+++ LiveFootball/LiveFootball/Views/LeagueStandingView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace LiveFootball.Views;
+
+/// <summary>
+///     Interaction logic for LeagueStandingView.xaml
+/// </summary>
+public partial class LeagueStandingView : UserControl
+{
+    public LeagueStandingView()
+    {
+        InitializeComponent();
+    }
+}
