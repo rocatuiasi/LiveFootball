f60c0632ef4fea592969396d4dbc76589fe80cfa
Ioan Varvara <ioan.varvara@student.tuiasi.ro>
2024-04-07 00:30:01 +0300

    Implementat Dinamic Butoane Meniu + Expander

---
+++ LiveFootball/LiveFootball/ViewModels/MenuViewModel.cs
@@ -0,0 +1,40 @@
+﻿using LiveFootball.Models;
+
+using System.Collections.ObjectModel;
+
+namespace LiveFootball.ViewModels;
+
+public class MenuViewModel : ViewModelBase
+{
+    #region Properties
+
+    public ObservableCollection<MenuModel> Leagues { get; set; }
+
+    public ObservableCollection<MenuModel> Competitions {  get; set; } 
+
+    #endregion
+
+    #region Constructors
+    public MenuViewModel() 
+    {
+        Leagues = new ObservableCollection<MenuModel>()
+        {
+            new("Premier League"),
+            new("La Liga"),
+            new("Bundesliga"),
+            new("Ligue 1"),
+            new("Serie A"),
+            new("Liga 1 - Superliga")
+        };
+
+        Competitions = new ObservableCollection<MenuModel>()
+        {
+            new("Champions League"),
+            new("Europa League"),
+            new("Conference League"),
+            new("Euro")
+        };
+    }
+
+    #endregion
+}
