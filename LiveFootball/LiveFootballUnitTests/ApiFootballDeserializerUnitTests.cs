0a60050d2d416aa1911d984787da30a454e085f9
Dani Roca <danielroca1702@gmail.com>
2024-05-26 23:42:03 +0300

    temp commit

---
+++ LiveFootball/LiveFootballUnitTests/ApiFootballDeserializerUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using ApiFootballDeserializer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiveFootballUnitTests;
+
+[TestClass]
+public class ApiFootballDeserializerUnitTests
+{
+    private IResultsDeserializer _resultsDeserializer;
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+
+    }
+}
---
+++ LiveFootball/LiveFootballUnitTests/FixturesDeserializerUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using ApiFootballDeserializer;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LiveFootballUnitTests;
+
+[TestClass]
+[TestSubject(typeof(FixturesDeserializer))]
+[TestCategory("Unit")]
+public class FixturesDeserializerUnitTests
+{
+    [TestMethod]
+    public async void Deserialize_ValidJson_ReturnsFixtureObject()
+    {
+        // Arrange
+        var json = "{\"id\": 1, \"homeTeam\": \"Team A\", \"awayTeam\": \"Team B\", \"date\": \"2022-01-01\"}";
+        var deserializer = new FixturesDeserializer();
+
+        // Act
+        var fixture = (await deserializer.Deserialize(json)).First();
+
+        // Assert
+        Assert.IsNotNull(fixture);
+        // Assert.AreEqual(1, fixture.);
+        Assert.AreEqual("Team A", fixture.HomeTeam.Name);
+        Assert.AreEqual("Team B", fixture.AwayTeam.Name);
+        // Assert.AreEqual(new DateTime(2022, 01, 01), fixture.Date);
+    }
+
+    [TestMethod]
+    public void Deserialize_InvalidJson_ThrowsException()
+    {
+        // Arrange
+        var json = "{\"id\": 1, \"homeTeam\": \"Team A\", \"awayTeam\": \"Team B\"}";
+        var deserializer = new FixturesDeserializer();
+
+        // Act & Assert
+        Assert.ThrowsException<Exception>(() => deserializer.Deserialize(json));
+    }
+}
